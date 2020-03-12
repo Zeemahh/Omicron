64c4938569fd42103b7abb345aa07d587ac1e096
Zeemah <zeemah@protonmail.ch>
2020-03-12 02:58:30 +0000

    dev: enhancement/changes on config

--- src/bot/config.ts
+++ src/bot/config.ts
@@ -1,11 +1,34 @@
+import { ColorResolvable, GuildChannel, Guild, Channel } from "discord.js";
+import { doesXExistOnGuild } from "./utils/functions";
+
 export const settings: {
     playerReports: {
         logs: string,
-        category: string
+        category: string,
+        newEmbed: {
+            color: ColorResolvable
+        },
+        deleteEmbed: {
+            color: ColorResolvable
+        }
     }
 } = {
     playerReports: {
         logs: '687441523972767785',
-        category: '686624560086253646'
+        category: '686624560086253646',
+        newEmbed: {
+            color: '#0B71A6'
+        },
+        deleteEmbed: {
+            color: '#CA9148'
+        }
     }
 };
+
+export function getReportLogsChannel(guild: Guild): GuildChannel {
+    const reportLogs: Channel = guild.channels.cache.get(settings.playerReports.logs);
+    if (doesXExistOnGuild(reportLogs, guild)) {
+        return reportLogs as GuildChannel;
+    }
+    return undefined;
+}
