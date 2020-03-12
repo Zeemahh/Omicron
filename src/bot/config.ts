5b4523402ddf063db1ddae9095970da7a2fd233b
Zeemah <zeemah@protonmail.ch>
2020-03-12 02:15:48 +0000

    feat/dev: config implementation for small stuff

---
+++ src/bot/config.ts
@@ -0,0 +1,11 @@
+export const settings: {
+    playerReports: {
+        logs: string,
+        category: string
+    }
+} = {
+    playerReports: {
+        logs: '687441523972767785',
+        category: '686624560086253646'
+    }
+};
