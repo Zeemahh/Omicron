9ee8fb3bd6de441daea276ff0c14bcc8c2d1c25b
Zeemah <zeemah@protonmail.ch>
2020-03-12 23:50:02 +0000

    dev/refactor: changes to config to make it easier to have multiple guilds

--- src/bot/config.default.ts
+++ src/bot/config.default.ts
@@ -1,10 +1,16 @@
-import { ColorResolvable, GuildChannel, Guild, Channel } from "discord.js";
-import { doesXExistOnGuild } from "./utils/functions";
+import { ColorResolvable, GuildChannel, Guild, Channel, TextChannel, MessageEmbed, CategoryChannel } from "discord.js";
+import { doesXExistOnGuild, embedAuthIcon } from "./utils/functions";
+import { client } from './bot';
+
+interface IChannelCfg {
+    guildId: string;
+    channelId: string;
+}
 
 export const settings: {
     playerReports: {
-        logs: string,
-        category: string,
+        logs: IChannelCfg[],
+        category: IChannelCfg[],
         newEmbed: {
             color: ColorResolvable
         },
@@ -14,21 +20,76 @@ export const settings: {
     }
 } = {
     playerReports: {
-        logs: 'LOG_CHANNEL_ID',
-        category: 'CATEGORY_ID',
+        logs: [
+            {
+                guildId: 'GUILD_ID',
+                channelId: 'CHANNEL_ID_RELATIVE_TO_GUILD'
+            }
+        ],
+        category: [
+            {
+                guildId: 'GUILD_ID',
+                channelId: 'CATEGORY_ID_RELATIVE_TO_GUILD'
+            }
+        ],
         newEmbed: {
-            color: 'A_COLOR'
+            color: '#0B71A6'
         },
         deleteEmbed: {
-            color: 'A_COLOR'
+            color: '#CA9148'
         }
     }
 };
 
 export function getReportLogsChannel(guild: Guild): GuildChannel {
-    const reportLogs: Channel = guild.channels.cache.get(settings.playerReports.logs);
-    if (doesXExistOnGuild(reportLogs, guild)) {
-        return reportLogs as GuildChannel;
+    for (const [ _, val ] of Object.entries(settings.playerReports.logs)) {
+        if (val.guildId === guild.id) {
+            const reportLogs: Channel = guild.channels.cache.get(val.channelId);
+            if (doesXExistOnGuild(reportLogs, guild)) {
+                return reportLogs as GuildChannel;
+            }
+        }
     }
+
     return undefined;
 }
+
+export function getReportCategory(guild: Guild): CategoryChannel {
+    for (const [ _, val ] of Object.entries(settings.playerReports.category)) {
+        if (val.guildId === guild.id) {
+            const reportCategory: GuildChannel = guild.channels.cache.get(val.channelId);
+            if (reportCategory instanceof CategoryChannel) {
+                return reportCategory as CategoryChannel;
+            }
+        }
+    }
+
+    return undefined;
+}
+
+client.on('channelDelete', (channel) => {
+    if (!(channel instanceof TextChannel)) {
+        return;
+    }
+
+    if (channel.parent.id === getReportCategory(channel.guild).id && channel.id !== getReportLogsChannel(channel.guild).id) {
+        if (channel.messages.cache.size > 0 && channel.lastMessage?.author.id !== client.user.id) {
+            const embed: MessageEmbed = new MessageEmbed()
+                .setAuthor('Alert | Report Deletion', embedAuthIcon)
+                .setColor('#FF4E00')
+                .setDescription(`Channel \`#${channel.name}\` (${channel.id}) was unexpectedly deleted. The last message was not sent by the bot and therefore wasn't properly processed.`)
+                .setTimestamp();
+
+            if (channel.lastMessage !== null) {
+                embed.addField('Last Message Author', channel.lastMessage.author.username);
+                embed.addField('Last Message Content', channel.lastMessage.content);
+            }
+
+            const logs: GuildChannel = getReportLogsChannel(channel.guild);
+            if (logs instanceof TextChannel) {
+                logs.send(embed);
+                return;
+            }
+        }
+    }
+});
