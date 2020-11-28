61c0060c1c40d3c68e4e426efb147869ecacb70d
zeemah <no_email_for_you@sad.com>
2020-11-28 18:07:34 +0000

    dev: feat: extend HGuild class

--- utils/classes/HGuild.ts
+++ utils/classes/HGuild.ts
@@ -1,18 +1,40 @@
 import { settings } from 'cluster';
-import { Guild, CategoryChannel, GuildChannel } from 'discord.js';
+import { Guild, CategoryChannel, GuildChannel, Collection, GuildMember, Role } from 'discord.js';
 import { ISettings } from '../../config';
 import { doesXExistOnGuild } from '../functions';
 
 export class HGuild {
-    protected data: Guild;
+    protected _data: Guild;
 
     constructor(guild: Guild) {
-        this.data = guild;
+        this._data = guild;
+    }
+
+    public get Channels(): Collection<string, GuildChannel> {
+        return this.__data.channels.cache;
+    }
+
+    public async Members(): Promise<Collection<string, GuildMember>> {
+        const members = await this.__data.members.fetch();
+        return members;
+    }
+
+    public async FetchSpecificMember(member: string): Promise<GuildMember> {
+        return await this.__data.members.fetch(member);
+    }
+
+    public async Bots(): Promise<Collection<string, GuildMember>> {
+        const members = await this.Members();
+        return members.filter(x => x.user.bot);
+    }
+
+    public get Roles(): Collection<string, Role> {
+        return this.__data.roles.cache;
     }
 
     public get Settings(): ISettings {
         for (const [ , val ] of Object.entries(settings)) {
-            if (val.guildId === this.data.id) {
+            if (val.guildId === this._data.id) {
                 return val;
             }
         }
@@ -24,7 +46,7 @@ export class HGuild {
         if (!this.Settings || this.Settings.tickets) return undefined;
 
         let category: GuildChannel;
-        category = this.data.channels.cache.get(this.Settings.tickets.category.channelId);
+        category = this._data.channels.cache.get(this.Settings.tickets.category.channelId);
         if (category instanceof CategoryChannel) {
             return <CategoryChannel> category;
         }
@@ -35,17 +57,41 @@ export class HGuild {
     public get TicketChannel(): GuildChannel {
         if (!this.Settings || !this.Settings.tickets) return undefined;
 
-        const channel = this.data.channels.cache.get(this.Settings.tickets.initChannel.channelId);
-        if (doesXExistOnGuild(channel, this.data)) {
+        const channel = this._data.channels.cache.get(this.Settings.tickets.initChannel.channelId);
+        if (doesXExistOnGuild(channel, this._data)) {
             return channel;
         }
 
         return undefined;
     }
 
+    public get Tickets() {
+        if (!this.Settings?.tickets) return undefined;
+
+        const ticketData = this.Settings.tickets;
+        const Category = this.Channels.get(ticketData.category.channelId);
+        const InitChannel = this.Channels.get(ticketData.initChannel.channelId);
+        const Logging = this.Channels.get(ticketData.logs.channelId);
+
+        return {
+            Category,
+            InitChannel,
+            Logging,
+            AllValid:
+                !!Category &&
+                !!InitChannel &&
+                !!Logging,
+            MessageContent: this.Settings.tickets?.category?.msgContent
+        }
+    }
+
     public get PromptMessage(): string {
         if (!this.Settings || !this.Settings.tickets) return undefined;
 
         return this.Settings.tickets.category?.msgContent;
+    }
+
+    public get __data(): Guild {
+        return this._data;
     }
 }
